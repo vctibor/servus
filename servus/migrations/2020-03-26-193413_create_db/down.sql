c1e17c7dad554617990b1bc69543b84fdb309924
Malky <vladimir.malky@gmail.com>
2020-04-01 17:44:03 +0200

    Completed create_db migration

--- servus/migrations/2020-03-26-193413_create_db/down.sql
+++ servus/migrations/2020-03-26-193413_create_db/down.sql
@@ -1,1 +1,4 @@
+drop table tx_log;
+drop table jobs;
+drop table machines;
 drop table users;
--- servus/src/lib/schema.rs
+++ servus/src/lib/schema.rs
@@ -1,7 +1,52 @@
 table! {
+    jobs (id) {
+        id -> Uuid,
+        name -> Text,
+        code -> Text,
+        description -> Nullable<Text>,
+        schedule -> Text,
+        target -> Uuid,
+        owner -> Uuid,
+        last_update -> Timestamp,
+        send_email -> Bool,
+    }
+}
+
+table! {
+    machines (id) {
+        id -> Uuid,
+        name -> Text,
+        username -> Text,
+        url -> Text,
+        port -> Int4,
+    }
+}
+
+table! {
+    tx_log (id) {
+        id -> Uuid,
+        success -> Bool,
+        time -> Timestamp,
+        message -> Text,
+        job -> Uuid,
+    }
+}
+
+table! {
     users (id) {
         id -> Uuid,
         name -> Text,
         email -> Nullable<Text>,
     }
 }
+
+joinable!(jobs -> machines (target));
+joinable!(jobs -> users (owner));
+joinable!(tx_log -> jobs (job));
+
+allow_tables_to_appear_in_same_query!(
+    jobs,
+    machines,
+    tx_log,
+    users,
+);
