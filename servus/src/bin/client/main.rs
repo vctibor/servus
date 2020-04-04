9b87e16424d0d540e8b2c8d3dffa104fd364380a
Malky <vladimir.malky@gmail.com>
2020-04-04 20:27:50 +0200

    Added REST API for Machine

--- servus/src/bin/client/main.rs
+++ servus/src/bin/client/main.rs
@@ -1,4 +1,5 @@
 mod user;
+mod machine;
 
 use actix_web::{web, middleware, App, HttpServer};
 use diesel::pg::PgConnection;
@@ -32,11 +33,19 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(middleware::Logger::default())
             .data(pool.clone())
+
             .service(web::resource("/api/user/list").route(web::get().to(user::list_users)))
             .service(web::resource("/api/user/get/{user_id}").route(web::get().to(user::get_user)))
             .service(web::resource("/api/user/create").route(web::post().to(user::create_user)))
             .service(web::resource("/api/user/update/{user_id}").route(web::post().to(user::update_user)))
             .service(web::resource("/api/user/delete/{user_id}").route(web::post().to(user::delete_user)))
+
+            .service(web::resource("/api/machine/list").route(web::get().to(machine::list_machines)))
+            .service(web::resource("/api/machine/get/{machine_id}").route(web::get().to(machine::get_machine)))
+            .service(web::resource("/api/machine/create").route(web::post().to(machine::create_machine)))
+            .service(web::resource("/api/machine/update/{machine_id}").route(web::post().to(machine::update_machine)))
+            .service(web::resource("/api/machine/delete/{machine_id}").route(web::post().to(machine::delete_machine)))
+            
             .service(fs::Files::new("/", "./static/").index_file("index.html"))
     })
         .bind(&bind)?
--- servus/src/lib/persistence/mod.rs
+++ servus/src/lib/persistence/mod.rs
@@ -4,4 +4,4 @@ pub mod job;
 
 pub use user::{add_user, get_users, get_user, update_user, delete_user};
 pub use machine::{add_machine, get_machines, get_machine, update_machine, delete_machine};
-//pub use job::{add_job, get_jobs, get_job, update_job, delete_job};
+pub use job::{add_job, get_jobs, get_job, update_job, delete_job};
