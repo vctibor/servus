a1e3b96d3a83682ee6654f83867089b5345d7873
Malky <vladimir.malky@gmail.com>
2020-04-05 15:25:56 +0200

    Fixed error handling

--- servus/src/lib/persistence/job.rs
+++ servus/src/lib/persistence/job.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use crate::entity::Job as JobEntity;
+use crate::entity::{AnyError, ServusError};
 use crate::schema::jobs;
 use crate::schema::jobs::dsl::*;
 use diesel::pg::PgConnection;
@@ -33,18 +34,19 @@ struct NewJob {
 }
 
 pub fn add_job(job: JobEntity, conn: &PgConnection)
-                -> Result<JobEntity, diesel::result::Error>
+                -> Result<JobEntity, AnyError>
 {
+    let target_id = job.target.id.ok_or(ServusError::new("Target ID not provided."))?;
+    let owner_id = job.owner.id.ok_or(ServusError::new("Owner ID not provided."))?;
+
     let job = Job {
         id: Uuid::new_v4(),
         name: job.name,
         code: job.code,
         description: job.description,
         schedule: job.schedule,
-
-        // TODO: remove unwrap - this is rather unsafe because we basically depend on client code
-        target: job.target.id.unwrap(),
-        owner: job.owner.id.unwrap(),
+        target: target_id,
+        owner: owner_id,
         last_update: job.last_update,
         send_email: job.send_email
     };
@@ -53,37 +55,39 @@ pub fn add_job(job: JobEntity, conn: &PgConnection)
         .values(&job)
         .execute(conn)?;
 
-    let owner_entity = super::user::get_user(job.owner, &conn)?;
+    let target_entity = super::machine::get_machine(job.target, &conn)?
+        .ok_or(ServusError::new("Invalid target ID."))?;
 
-    let target_entity = super::machine::get_machine(job.target, &conn)?;
-
+    let owner_entity = super::user::get_user(job.owner, &conn)?
+        .ok_or(ServusError::new("Invalid owner ID."))?;
+        
     Ok(JobEntity {
         id: Some(job.id),
         name: job.name,
         code: job.code.to_owned(),
         description: job.description.to_owned(),
         schedule: job.schedule.to_owned(),
-
-        // TODO: remove unwrap - this is pretty safe because it's FK enforced by database
-        target: target_entity.unwrap(),
-        owner: owner_entity.unwrap(),
+        target: target_entity,
+        owner: owner_entity,
         last_update: job.last_update,
         send_email: job.send_email
     })
 }
 
 pub fn get_jobs(conn: &PgConnection)
-                -> Result<Vec<JobEntity>, diesel::result::Error>
+                -> Result<Vec<JobEntity>, AnyError>
 {
     let job_table: Vec<Job> = jobs.load::<Job>(conn)?;
 
     let mut entities = Vec::with_capacity(job_table.len());
 
     for job in job_table.iter() {
+            
+        let target_entity = super::machine::get_machine(job.target, &conn)?
+            .ok_or(ServusError::new("Invalid target ID."))?;
 
-        let owner_entity = super::user::get_user(job.owner, &conn)?;
-
-        let target_entity = super::machine::get_machine(job.target, &conn)?;
+        let owner_entity = super::user::get_user(job.owner, &conn)?
+            .ok_or(ServusError::new("Invalid owner ID."))?;            
 
         entities.push(JobEntity {
             id: Some(job.id),
@@ -91,10 +95,8 @@ pub fn get_jobs(conn: &PgConnection)
             code: job.code.to_owned(),
             description: job.description.to_owned(),
             schedule: job.schedule.to_owned(),
-
-            // TODO: remove unwrap - this is pretty safe because it's FK enforced by database
-            target: target_entity.unwrap(),
-            owner: owner_entity.unwrap(),
+            target: target_entity,
+            owner: owner_entity,
             last_update: job.last_update,
             send_email: job.send_email
         });
@@ -104,7 +106,7 @@ pub fn get_jobs(conn: &PgConnection)
 }
 
 pub fn get_job(uid: Uuid, conn: &PgConnection)
-                -> Result<Option<JobEntity>, diesel::result::Error>
+                -> Result<Option<JobEntity>, AnyError>
 {
     let job = jobs
         .filter(id.eq(uid))
@@ -114,20 +116,20 @@ pub fn get_job(uid: Uuid, conn: &PgConnection)
     match job {
         Some(job) => {
 
-            let owner_entity = super::user::get_user(job.owner, &conn)?;
-
-            let target_entity = super::machine::get_machine(job.target, &conn)?;
-
+            let target_entity = super::machine::get_machine(job.target, &conn)?
+                .ok_or(ServusError::new("Invalid target ID."))?;
+    
+            let owner_entity = super::user::get_user(job.owner, &conn)?
+                .ok_or(ServusError::new("Invalid owner ID."))?;
+            
             Ok(Some(JobEntity {
                 id: Some(job.id),
                 name: job.name,
                 code: job.code,
                 description: job.description,
                 schedule: job.schedule,
-                
-                // TODO: remove unwrap - this is pretty safe because it's FK enforced by database
-                target: target_entity.unwrap(),
-                owner: owner_entity.unwrap(),
+                target: target_entity,
+                owner: owner_entity,
                 last_update: job.last_update,
                 send_email: job.send_email
             }))
@@ -137,27 +139,30 @@ pub fn get_job(uid: Uuid, conn: &PgConnection)
 }
 
 pub fn update_job(job: JobEntity, job_id: Uuid, conn: &PgConnection)
-                   -> Result<usize, diesel::result::Error>
+                   -> Result<usize, AnyError>
 {
+    let target_id = job.target.id.ok_or(ServusError::new("Target ID not provided."))?;
+    let owner_id = job.owner.id.ok_or(ServusError::new("Owner ID not provided."))?;
+
     let job = Job {
         id: job_id,
         name: job.name,
         code: job.code,
         description: job.description,
         schedule: job.schedule,
-
-        // TODO: remove unwrap - this is rather unsafe because we basically depend on client code
-        target: job.target.id.unwrap(),
-        owner: job.owner.id.unwrap(),
+        target: target_id,
+        owner: owner_id,
         last_update: job.last_update,
         send_email: job.send_email
     };
 
-    diesel::update(jobs::table).set(&job).execute(conn)
+    let res = diesel::update(jobs::table).set(&job).execute(conn)?;
+    Ok(res)
 }
 
 pub fn delete_job(uid: Uuid, conn: &PgConnection)
                    -> Result<usize, diesel::result::Error>
 {
-    diesel::delete(jobs.filter(id.eq(uid))).execute(conn)
+    let res = diesel::delete(jobs.filter(id.eq(uid))).execute(conn)?;
+    Ok(res)
 }
