dcdd69829a8c8b6cbac9c55ea0a32885b39af214
Malky <vladimir.malky@gmail.com>
2020-04-05 22:55:23 +0200

    Custom checkbox

--- servus/static/js/site.js
+++ servus/static/js/site.js
@@ -35,6 +35,40 @@ var myApp = angular.module('myApp', ['ngRoute'])
         .then(function (response) {
             $scope.machines = response.data;
         });
+
+
+    
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
+    var btnClick = function() {
+        alert("test");
+    }
+
 })
 
 .controller('UsersController', function($scope, $routeParams, $http) {
