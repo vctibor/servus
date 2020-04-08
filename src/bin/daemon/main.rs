84e1da1154845f52505c3c8612b00cc1a64c508d
Malky <vladimir.malky@gmail.com>
2020-04-08 21:58:39 +0200

    Daemon work

--- src/bin/daemon/main.rs
+++ src/bin/daemon/main.rs
@@ -3,7 +3,7 @@ use std::net::{TcpStream};
 use ssh2::Session;
 
 use servus::entity::Job as JobEntity;
-use servus::entity::{AnyError, ServusError};
+use servus::entity::{AnyError};
 
 use diesel::pg::PgConnection;
 use diesel::r2d2::{self, ConnectionManager};
@@ -15,6 +15,10 @@ use tokio::time::delay_for;
 use job_scheduler::{JobScheduler, Job};
 use std::time::Duration;
 
+
+/// Number of milliseconds to sleep between every job scheduler check.
+/// Perhaps should be configurable.
+const REFRESH_RATE: u64 = 500;
 
 
 /// Executes provided command on remote machine using ssh.
@@ -53,61 +57,85 @@ pub fn exec_remote(username: &str, url: &str, port: i32, command: &str)
     return Ok(output);
 }
 
-pub fn exec_remote_job(job: JobEntity) -> Result<String, AnyError> {
-    let username = job.target.username;
-    let url = job.target.url;
+pub fn exec_remote_job(job: &JobEntity) -> Result<String, AnyError> {
+    let username = job.target.username.clone();
+    let url = job.target.url.clone();
     let port = job.target.port;
-    let command = job.code;
+    let command = job.code.clone();
     exec_remote(&username, &url, port, &command)
 }
 
 #[tokio::main]
-async fn main() {
-    
-    // while true:
-    //     for every job:
-    //         - compare last_update in memory vs. last_update in db
-    //         - reschedule jobs that were updated since last scheduling
+async fn main() -> Result<(), AnyError>
+{
+    //
+    //  let scheduled_jobs = Map<job_id, scheduled_job_id>   // have map structure mapping job "type" to scheduled "instance"
+    //
+    //  while true:
+    //
+    //      let jobs = servus::persistence::get_jobs()           // obtain list of all scheduled jobs
     // 
-    //     check if any job is scheduled for this time
-    //     if yes:
-    //         - get job details
-    //         - execute job
-    //         - log stderr, stdout, exit status
-    
-    println!("This is daemon.");
+    //      if job not in scheduled_jobs:
+    //          -- get job details
+    //          -- schedule job
+    //      elif job.last_update > scheduled_job.last_update:
+    //          -- remove old instance
+    //          -- reschedue job
+    //      else:
+    //          -- skip
+    //  
+    //  job_scheduler.tick()
+    //      -- log stdout, stderr, exit status
 
 
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
+    let database_url = env::var("DATABASE_URL")?;
 
     let manager = ConnectionManager::<PgConnection>::new(database_url);
 
-    let pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.");
-
-    let conn = pool.get().expect("Failed to obtain connection from pool.");
-
-    let mut sched = JobScheduler::new();
-
-    sched.add(Job::new("1/3 * * * * *".parse().unwrap(), || {
-        /*
-        let username = "malky";
-        let url = "dockerhost.malkynet";
-        let port = 22;
-        let command = "ls -lh";
-        let res = exec_remote(username, url, port, command);
-        println!("{}", res)
-        */
-        println!("test");
-    }));
-
+    let pool = r2d2::Pool::builder().build(manager)?;
+        
+    let mut job_scheduler = JobScheduler::new();
+        
     loop {
-        sched.tick();
-        delay_for(Duration::from_millis(500)).await;
+        
+        let conn = pool.get().expect("Failed to obtain connection from pool.");
+
+        let jobs = servus::persistence::get_jobs(&conn)?;
+
+        for job in jobs {
+
+            let schedule = "* 1/3 * * * *".parse();
+
+            if schedule.is_err() {
+                println!("Failed to parse schedule for job {}", job.id.unwrap());
+                continue;
+            }
+
+            let schedule = schedule.unwrap();
+            
+            // We will have to keep map of <job_id, scheduled_job_id>,
+            //   and rescheduled every updated job.
+            let scheduled_job_id = job_scheduler.add(Job::new(schedule, || {
+                
+                /*
+                let username = "malky";
+                let url = "dockerhost.malkynet";
+                let port = 22;
+                let command = "ls -lh";
+                let res = exec_remote(username, url, port, command);
+                println!("{}", res)
+                */
+
+                let exec_res = exec_remote_job(&job);
+
+            }));
+        }
+
+        job_scheduler.tick();
+
+        delay_for(Duration::from_millis(REFRESH_RATE)).await;
     }
     
 }
