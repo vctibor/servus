4cbe6a1cd7f3e412ccdd84cffaa1e6843e00a33f
Malky <vladimir.malky@gmail.com>
2020-06-17 22:28:09 +0200

    Fixing ssh agent

--- src/bin/servus/main.rs
+++ src/bin/servus/main.rs
@@ -30,28 +30,26 @@ async fn redirect_to_index() -> HttpResponse {
     HttpResponse::PermanentRedirect().header(http::header::LOCATION, "/").finish()
 }
 
-// testing ssh agent
-fn main() {
-
-    use ssh2::Session;
-
-    // Almost all APIs require a `Session` to be available
-    let sess = Session::new().unwrap();
-    let mut agent = sess.agent().unwrap();
-
-    // Connect the agent and request a list of identities
-    agent.connect().unwrap();
-    agent.list_identities().unwrap();
-
-    for identity in agent.identities().unwrap() {
-        println!("{}", identity.comment());
-        let pubkey = identity.blob();
-    }
-}
-
-/*
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+
+    {
+        use ssh2::Session;
+
+        // Almost all APIs require a `Session` to be available
+        let sess = Session::new().unwrap();
+        let mut agent = sess.agent().unwrap();
+    
+        // Connect the agent and request a list of identities
+        agent.connect().unwrap();
+        agent.list_identities().unwrap();
+    
+        for identity in agent.identities().unwrap() {
+            println!("{}", identity.comment());
+            let pubkey = identity.blob();
+        }
+    }
+
     
     println!("hello world");
 
